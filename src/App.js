cc27c0f581b411ede901465403368d86118b10b3
Sebastian <sebastian.valencian@gmail.com>
2019-06-17 23:16:44 -0500

    [Feature]Install Axios, create a get call , modify state

--- src/App.js
+++ src/App.js
@@ -4,26 +4,33 @@ import CardList from "./CardList";
 import Form from "./Form";
 
 class App extends React.Component {
-  testData = [
-    {
-      name: "Dan Abramov",
-      avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
-      company: "@facebook"
-    },
-    {
-      name: "Sophie Alpert",
-      avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-      company: "Humu"
-    },
-    {
-      name: "Sebastian Markbåge",
-      avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-      company: "Facebook"
-    }
-  ];
+  // testData = [
+  //   {
+  //     name: "Dan Abramov",
+  //     avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+  //     company: "@facebook"
+  //   },
+  //   {
+  //     name: "Sophie Alpert",
+  //     avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+  //     company: "Humu"
+  //   },
+  //   {
+  //     name: "Sebastian Markbåge",
+  //     avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+  //     company: "Facebook"
+  //   }
+  // ];
 
   state = {
-    profiles: this.testData
+    // profiles: this.testData
+    profiles: []
+  };
+
+  addNewProfile = profileData => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
   };
 
   render() {
@@ -31,7 +38,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">The Github Card App</header>
 
-        <Form />
+        <Form onSubmit={this.addNewProfile}/>
 
         <CardList testData={this.state.profiles} />
       </div>
--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,21 @@
 import React from "react";
+import Axios from "axios";
 
 class Form extends React.Component {
   state = {
     userName: ""
   };
 
-  handleSubmit = event => {
+  // GitHub usernames: gaearon, sophiebits, sebmarkbage, bvaughn
+  
+  handleSubmit = async event => {
     event.preventDefault();
     console.log(this.state.userName);
+    const resp = await Axios.get(`https://api.github.com/users/${this.state.userName}`);
+    this.props.onSubmit(resp.data);
+    this.setState({
+        userName: ''
+    });
   };
 
   handleOnChange = event => {
