44227d22810f46b389b70db1ededd8fe6bd55e6c
Sebastian <sebastian.valencian@gmail.com>
2019-06-17 22:47:36 -0500

    [Feature] Add styles

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ class Card extends React.Component {
     const profile = this.props;
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} />
+        <img src={profile.avatar_url} alt={profile.avatar_url} />
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
